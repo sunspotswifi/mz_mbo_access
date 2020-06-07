70024f2cbf702a71a7af13be746851a33871b45f
Mike iLL Kilmer <mike@mzoo.org>
2020-06-07 09:02:06 -0500

    Update shortcode descriptions.

--- inc/backend/class-settings-page.php
+++ inc/backend/class-settings-page.php
@@ -86,19 +86,17 @@ class Settings_Page {
         $return = '';
         $return .= '<p>'.sprintf('[%1$s] %2$s [%3$s]', 'mbo-client-access', __("Restricted content here between both tags", 'mz-mindbody-api'), '/mbo-client-access').'</p>';
         $return .= "<ul>";
-        $return .= "<li><strong>level_2_redirect</strong>: " . __("(url string) URL to redirect users to with membership (service) matching membership_types attribute.", 'mz-mindbody-api')."</li>";
-        $return .= "<li><strong>contract_redirect</strong>: " . __("((url string) URL to redirect users to with Contract (membership) matching contract_types attribute. ", 'mz-mindbody-api')."</li>";
+        $return .= "<li><strong>level_1_redirect</strong>: " . __("(url string) URL to redirect users with level one access.", 'mz-mindbody-api')."</li>";
+        $return .= "<li><strong>level_2_redirect</strong>: " . __("(url string) URL to redirect users with level two access.", 'mz-mindbody-api')."</li>";
         $return .= "<li><strong>denied_redirect</strong>: " . __("((url string) URL to redirect users to who are logged in but don't have access.", 'mz-mindbody-api')."</li>";
-        $return .= "<li><strong>membership_types</strong>: " . __('(string) Comma-separated string of MBO "contract" (membership) types required for access.', 'mz-mindbody-api')."</li>";
-        $return .= "<li><strong>contract_types</strong>: " . __('(string) Comma-separated string of MBO "membership" (service) types required for access.', 'mz-mindbody-api')."</li>";
         $return .= "<li><strong>call_to_action</strong>: " . __("(string) Message inviting user to submit form.", 'mz-mindbody-api')."</li>";
         $return .= "<li><strong>denied_message</strong>: " . __("(string) Message preceding list of items required for access.", 'mz-mindbody-api')."</li>";
         $return .= "<li><strong>access_expired</strong>: " . __("(string) Message alerting client that access has expired.", 'mz-mindbody-api')."</li>";
-        $return .= "<li><strong>access_levels</strong>: " . sprintf(__('(int/list) Levels of access required to access content %1$d, %2$d or %3$s', 'mz-mindbody-api'), 1, 2, "\"1, 2\"")."</li>";
+        $return .= "<li><strong>access_levels</strong>: " . sprintf(__('(int/list) (Default %1$d) Levels of access required to access content %1$d, %2$d or %3$s', 'mz-mindbody-api'), 1, 2, "\"1, 2\"")."</li>";
         $return .= "</ul>";
         $return .= sprintf('[%1$s %2$s]%3$s[%4$s]', 'mbo-client-access', 'memberships="Corporate Membership, Monthly Membership - Gym Access"', 'Restricted Content', '/mbo-client-access');
         $return .= '<h3>'. __('Note', 'mz-mindbody-api') . '</h3>';
-        $return .= sprintf(__('If %1$s %2$s and %3$s are included, the login form will redirect to one of those urls instead of revealing content. Content, if any, between shortcode tags will display when page is reloaded by logged in client.', 'mz-mindbody-api'), '<code>contract_redirect</code>', '<code>level_2_redirect</code>', '<code>denied_redirect</code>');
+        $return .= sprintf(__('If %1$s %2$s or %3$s are included, the login form will redirect to one of those urls instead of revealing content. Content, if any, between shortcode tags will display when page is reloaded by logged in client.', 'mz-mindbody-api'), '<code>contract_redirect</code>', '<code>level_2_redirect</code>', '<code>denied_redirect</code>');
         return $return;
     }
 
