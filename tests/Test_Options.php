1848163f57bbacd2d7359f48a05eae222006003e
Mike iLL Kilmer <mike@mzoo.org>
2020-06-12 16:12:35 -0500

    Placeholders.

---
+++ tests/Test_Options.php
@@ -0,0 +1,27 @@
+<?php
+require_once('MZMBO_WPUnitTestCase.php');
+
+class Test_Options
+{
+    public static $_MYSOURCENAME = 'MediaZoo'; // Intensity TOTALTENNISANDFITNESSLLC
+    public static $_MYPASSWORD = 'Bi0ZUro4xZZK77FcHzUH1KY8Tpg='; // Intensity UBDIf6xZb9Sjo2Q8l/Xx3H+s9hs=
+    
+    public static $_MYAPPNAME = 'MediaZoo-1'; // Intensity INTENSITYCLUBSITE
+    public static $_MYAPIKEY = 'a3f5be6229744000b9bc25f603e80c45'; // Intensity 4a0d0ee723c24dbabbf099b9c7dd5dfa Intensity ID 67470, Uru SiteID 43474
+    public static $_MYEVENTIDS = '4,6,20,28,18,27';
+    public static $_MYACCESSTOKEN = '';
+    
+    // Test Client
+    public static $_FIRSTNAME = 'Melody';
+    public static $_LASTNAME = 'Macher';
+    public static $_CLIENTEMAIL = 'mike@madhappy.com';
+    public static $_CLIENTSTATE = 'FL';
+    public static $_CLIENTBIRTHDATE = '10/05/1969';
+    public static $_CLIENTID = '';
+    // For now manually added:
+    public static $_CLIENTPASSWORD = 'A3f5be622974';
+    // Site IDs: Uru 43474, Medicine: 437889
+    
+}
+
+?>
